571659b6595f6341b38b01bc538f17047cf7a501
Jett Lai <0908jettlai@gmail.com>
2023-12-01 15:53:54 +0800

    update new

--- MauiApp1/Question2.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MauiApp1;
-
-public partial class Question2 : ContentPage
-{
-	public Question2()
-	{
-		InitializeComponent();
-	}
-}
--- MauiApp1/Question3.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MauiApp1;
-
-public partial class Question3 : ContentPage
-{
-	public Question3()
-	{
-		InitializeComponent();
-	}
-}
